1c1023e2c87ff93e65c7230fdd594d7dfad1efe5
Nkosi Phillip <internap627@gmail.com>
2018-09-14 06:23:33 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -2,17 +2,15 @@
 
 def prime?(n)
 if n < 0
-  return false 
-end
+  return false
+  end
 
   for d in 2..(n - 1)
-  if (n % d) == 0 
+   if (n % d) == 0 || n < 0
     return false
    end
   end
 
   true
-  
- 
 
  end
